9f43f73ad3c49b53b43b9579007fc58c366e2132
yashashav-dk <ydk61999@gmail.com>
2025-01-19 01:48:37 +0530

    removed old code

---
+++ Basics/BMath/BMath_1_CountAllDigitsOfANumber.java
@@ -0,0 +1,8 @@
+package Basics.BMath;
+
+public class BMath_1_CountAllDigitsOfANumber {
+    public static void main(String[] args) {
+        System.out.println("Hello World88");
+
+    }
+}
---
+++ Basics/BRecursion/BRecursion_1_Test.java
@@ -0,0 +1,7 @@
+package Basics.BRecursion;
+
+public class BRecursion_1_Test {
+    public static void main(String[] args) {
+        System.out.println("Testing Recursion");
+    }
+}
