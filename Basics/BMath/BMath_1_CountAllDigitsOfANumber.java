ee76f21483847c2aa4be27cc8f1af3a9926dbdfa
yashashav-dk <ydk61999@gmail.com>
2025-01-19 02:02:00 +0530

    working tasks.json finally

--- Basics/BMath/BMath_1_CountAllDigitsOfANumber.java
+++ Basics/BMath/BMath_1_CountAllDigitsOfANumber.java
@@ -1,8 +1,23 @@
 package Basics.BMath;
 
+import java.util.Scanner;
+
 public class BMath_1_CountAllDigitsOfANumber {
     public static void main(String[] args) {
-        System.out.println("Hello World88");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = countDigits(number);
+        System.out.println("Number of digits in " + number + " is: " + count);
+        scanner.close();
+    }
 
+    private static int countDigits(int number) {
+        if (number == 0) return 1;
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
     }
 }
---
+++ Basics/BStrings/BStrings_1_TestStr.java
@@ -0,0 +1,7 @@
+package Basics.BStrings;
+
+public class BStrings_1_TestStr {
+    public static void main(String[] args) {
+        System.out.println("Hello All");
+    }
+}
