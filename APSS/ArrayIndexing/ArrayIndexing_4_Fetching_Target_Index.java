ff9abc99f88b004a632db7572bc6a1c421d1f08d
yashashav-dk <ydk61999@gmail.com>
2025-01-31 17:40:56 +0530

    added APSS 4: finding target

--- APSS/ArrayIndexing/ArrayIndexing_4_Fetching_Target_Index.java
+++ APSS/ArrayIndexing/ArrayIndexing_4_Fetching_Target_Index.java
@@ -5,8 +5,31 @@ import java.util.Scanner;
 
 public class ArrayIndexing_4_Fetching_Target_Index {
     public static void main(String[] args) {
-        Scanner keyboardScanner = new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+        int arrSize = scan.nextInt();
+        int target = scan.nextInt();
+        int[] arr = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        // search for targer in arr
+        int indexAt = 0;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                found = true;
+                indexAt = i;
+            }
+        }
+
+        if(found) {
+            System.out.println(indexAt + 1);
+        } else {
+            System.out.println(-1);
+        }
 
     }
 }
