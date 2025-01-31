fa2326fb10bbefc2ff4927b026cec47339f8e8a1
yashashav-dk <ydk61999@gmail.com>
2025-01-31 17:23:21 +0530

    added APSS problem 2

--- APSS/ArrayIndexing/ArrayIndexing_2_Iterate_Array_Reverse.java
+++ APSS/ArrayIndexing/ArrayIndexing_2_Iterate_Array_Reverse.java
@@ -1,10 +1,32 @@
 package APSS.ArrayIndexing;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayIndexing_2_Iterate_Array_Reverse {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        for (int i = arr.length - 1; i > -1; i--) {
-            System.out.print(arr[i] + " ");
+        // static input 
+        Scanner scan = new Scanner(System.in);
+        // int arrSize = scan.nextInt();
+        // int[] arr = new int[arrSize];
+
+        // for (int i = 0; i < arrSize; i++) {
+        //     arr[i] = scan.nextInt();
+        // }
+        // // printing it in the reverse order
+        // for (int i = arrSize - 1; i > -1; i--) {
+        //     System.out.print(arr[i] + " ");
+        // }
+
+
+        // dynamic input
+        ArrayList<Integer> al = new ArrayList<>();
+        while(scan.hasNextInt()) {
+            al.add(scan.nextInt());
+        }
+
+        for (int i = al.size() - 1; i > -1; i--) {
+            System.out.print(al.get(i) + " ");
         }
     }
 }
