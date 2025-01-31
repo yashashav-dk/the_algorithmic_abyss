b1396021afecac3374f7d976e650f179fea79cbc
yashashav-dk <ydk61999@gmail.com>
2025-01-31 17:32:35 +0530

    added APSS 4

--- APSS/ArrayIndexing/ArrayIndexing_3_Fetch_Every_2nd_Element.java
+++ APSS/ArrayIndexing/ArrayIndexing_3_Fetch_Every_2nd_Element.java
@@ -1,9 +1,20 @@
 package APSS.ArrayIndexing;
 
+import java.util.Scanner;
+
 public class ArrayIndexing_3_Fetch_Every_2nd_Element {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4,5 ,6, 7, 8, 9};
-        for (int i = 0; i < arr.length; i = i + 2) {
+        // stick to static array from now
+        Scanner scan = new Scanner(System.in);
+        int arrSize = scan.nextInt();
+        int[] arr = new int[arrSize];
+
+        for(int i = 0; i < arrSize; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        // fetching every 2nd element
+        for(int i = 0; i < arrSize; i = i + 2) {
             System.out.print(arr[i] + " ");
         }
     }
