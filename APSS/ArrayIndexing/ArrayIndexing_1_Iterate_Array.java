6424c432b74759e9d191b60434c376ed0721b414
yashashav-dk <ydk61999@gmail.com>
2025-01-31 17:06:50 +0530

    added basic i/o code

--- APSS/ArrayIndexing/ArrayIndexing_1_Iterate_Array.java
+++ APSS/ArrayIndexing/ArrayIndexing_1_Iterate_Array.java
@@ -1,10 +1,33 @@
 package APSS.ArrayIndexing;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayIndexing_1_Iterate_Array {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5}; // declare and initialise at the same time
+        Scanner keyboard = new Scanner(System.in);
+
+        // dynamic input
+
+        // ArrayList<Integer> alist = new ArrayList<Integer>();
+        // while (keyboard.hasNextInt()) {
+        //     alist.add(keyboard.nextInt());
+        // }
+        // for (Integer integer : alist) {
+        //     System.out.println(integer);
+        // }
+
+        // static input
+
+        int arrSizze = keyboard.nextInt();
+        int[] arr = new int[arrSizze];
         for (int i = 0; i < arr.length; i++) {
+            arr[i] = keyboard.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+
     }
 }
---
+++ APSS/ArrayIndexing/ArrayIndexing_4_Fetching_Target_Index.java
@@ -0,0 +1,12 @@
+package APSS.ArrayIndexing;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayIndexing_4_Fetching_Target_Index {
+    public static void main(String[] args) {
+        Scanner keyboardScanner = new Scanner(System.in);
+        
+
+    }
+}
