bc371a1dc409e3e8479656443d59b825a9ff6cb8
yashashav-dk <ydk61999@gmail.com>
2025-02-03 23:27:23 +0530

    Added Traversing 2D Matrix

---
+++ APSS/ArrayIndexing/ArrayIndexing_6_Traverse_2D_Matrix.java
@@ -0,0 +1,10 @@
+package APSS.ArrayIndexing;
+
+import java.util.Scanner;
+
+public class ArrayIndexing_6_Traverse_2D_Matrix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
