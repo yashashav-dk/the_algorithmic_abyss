66d4c836bfd303d116dca8b21b42400d28864c36
yashashav-dk <35463938+yashashav-dk@users.noreply.github.com>
2025-12-24 21:32:45 -0800

    Time: 3 ms (49.9%), Space: 18.6 MB (69.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        Approach 1: Use hashmap to find the missing element
-        x + y = target
-        x = target - y
-        loop for each element in nums:
-            check if x in map
-        
-        Approach 2: Use two points at extremes and converge
-        i and j pointer at 0th idx and -1th idx. But sort beforehand.   
-        [2 7 11 15]
-        target = 9
+        1.  x + y = target; lets make it target - y = x; let's find x in a table/set
+
         """
-        pairs = sorted((v, i) for i, v in enumerate(nums))
-        lo, hi = 0, len(nums) - 1
 
-        while (lo < hi):
-            ith = pairs[lo][0]
-            jth = pairs[hi][0]
-            if (ith + jth == target):
-                return [pairs[lo][1], pairs[hi][1]]
-            elif (ith + jth > target):
-                hi -= 1
+        seen = {}
+        target_list = []
+        for idx in range(len(nums)):
+            num = nums[idx]
+            diff = target - num
+            if diff in seen:
+                target_list.append(seen[diff])
+                target_list.append(idx)
             else:
-                lo += 1
-        return []
-        
+                seen[num] = idx
 
-
-        
-
-        
+        return target_list
